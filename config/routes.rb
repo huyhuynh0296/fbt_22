f705dcd20fdb1b9de286297e9d54a7566e972453
huyhuynh0296 <huyhuynhdtu@gmail.com>
2018-05-06 16:46:43 +0700

    signup-login-logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   get "/gioithieu", to: "static_pages#gioithieu"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+<<<<<<< HEAD
   get "/signup", to: "users#new"
+=======
+  delete "/logout", to: "sessions#destroy"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users
+>>>>>>> a5f22ce... signup-login-logout
 end
--- db/migrate/20180501042714_create_users.rb
+++ db/migrate/20180501042714_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+<<<<<<< HEAD
       t.string :username
       t.string :email
       t.string :password
@@ -8,6 +9,14 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :numberphone
       t.integer :role, default: 0
       t.string :images
+=======
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :role, default: 0
+      t.string :avatar
+      t.string :remember_digest
+>>>>>>> a5f22ce... signup-login-logout
 
       t.timestamps
     end
